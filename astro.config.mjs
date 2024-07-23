28945d6675c0dc4a076c6c18a4ef9c57f15d4768
siluat <siluat@gmail.com>
2024-07-23 14:13:01 +0900

    ➕ Add Tailwind CSS configuration and dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
 import storyblok from "@storyblok/astro";
 import { loadEnv } from "vite";
-
+import tailwind from "@astrojs/tailwind";
 const env = loadEnv("", process.cwd(), "STORYBLOK");
 
+// https://astro.build/config
 export default defineConfig({
   integrations: [
     storyblok({
@@ -17,5 +18,6 @@ export default defineConfig({
         region: "us",
       },
     }),
+    tailwind(),
   ],
 });
