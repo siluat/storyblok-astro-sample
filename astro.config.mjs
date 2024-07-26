cd3a31eab94404677f9d89b2db7bea49eb95ef33
siluat <siluat@gmail.com>
2024-07-26 14:40:56 +0900

    💄 Add bird icon to header

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,41 +3,28 @@ import storyblok from "@storyblok/astro";
 import { loadEnv } from "vite";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-
+import react from "@astrojs/react";
 const env = loadEnv("", process.cwd(), "STORYBLOK");
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    storyblok({
-      accessToken: env.STORYBLOK_TOKEN,
-      components: {
-        blogPost: "storyblok/BlogPost",
-        blogPostList: "storyblok/BlogPostList",
-        page: "storyblok/Page",
-      },
-      apiOptions: {
-        region: "us",
-      },
-    }),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    icon({
-      include: {
-        tabler: ["*"],
-        "flat-color-icons": [
-          "template",
-          "gallery",
-          "approval",
-          "document",
-          "advertising",
-          "currency-exchange",
-          "voice-presentation",
-          "business-contact",
-          "database",
-        ],
-      },
-    }),
-  ],
+  integrations: [storyblok({
+    accessToken: env.STORYBLOK_TOKEN,
+    components: {
+      blogPost: "storyblok/BlogPost",
+      blogPostList: "storyblok/BlogPostList",
+      page: "storyblok/Page"
+    },
+    apiOptions: {
+      region: "us"
+    }
+  }), tailwind({
+    applyBaseStyles: false
+  }), icon({
+    include: {
+      tabler: ["*"],
+      "flat-color-icons": ["template", "gallery", "approval", "document", "advertising", "currency-exchange", "voice-presentation", "business-contact", "database"]
+    }
+  }), react()]
 });
